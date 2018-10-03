ced47e2b5698cae2048d0ff5d91261645d61100c
tiffany.jansen <tjansen15@wou.edu>
2018-10-02 20:47:38 -0700

    Fixing reg Expression

--- HW2/feat.js
+++ HW2/feat.js
@@ -1,16 +1,34 @@
 function error(){
-    document.getElementById("error").style.visibility = "visible";
+    var node = document.getElementById("error");
+    var heading = document.createElement("h2");
+    var message = document.createTextNode("ERROR! Input received was not a polynomial. Please try again.");
+    node.style.background = "red";
+    node.style.color = "white";
+    node.style.padding = "10px";
+    heading.appendChild(message);
+    node.appendChild(heading);
 }
 function enter(){
     var words = document.getElementById("function").value;
+    document.getElementById("function").value = "";
     console.log(words);
     if(match(words) == null){
         error();
     }
+    else{
+        matched(words);
+    }
 }
 function match(s){
-    var reg = /\d*x(?=\^?\+?\-?\d+){1,}/
-    var found = s.match(reg);
+    var regs = /\d+x\^\d+/
+    var found = s.match(regs);
     console.log(found);
     return found;
+}
+function matched(fx){
+    document.getElementById("text").remove();
+    document.getElementById("text2").remove();
+    document.getElementById("func").remove();
+    document.getElementById("error").remove();
+    console.log(fx);
 }
