f2324f5350ad132bcc327c2c1f7ccd7e53fa7d02
tiffany.jansen <tjansen15@wou.edu>
2018-10-03 15:48:38 -0700

    Made table workgit add .

--- HW2/feat.js
+++ HW2/feat.js
@@ -40,11 +40,15 @@ function matched(fx){
 function findInt(fx){
     var reg = /x+/g;
     if(reg.test(fx)==true){
-
+        //JUST NEED TO DO THIS PART!!
     }
     else{
         var numReg = /\d+/;
         var found = fx.match(numReg);
+        if(found == 0){
+            console.log(found);
+            return found;
+        }
         console.log(found + 'x');
         return found + 'x';
     }
@@ -52,7 +56,7 @@ function findInt(fx){
 function findDer(fx){
     var reg = /x+/g;    
     if(reg.test(fx)==true){
-
+        //JUST NEED TO DO THIS PART!!
     }
     else{
         console.log(0);
@@ -60,17 +64,33 @@ function findDer(fx){
     }
 }
 function createTable(int, fx, firDer, secDer){
-    var data = [int, fx, firDer, secDer];
-    var columnHeadings = ["Integral F(x)", "Function f(x)", "First Derivative f'(x)", "Second Derivative f''(x)"];
+    var data =[{
+        Integral: int,
+        Function: fx,
+        FirstDer: firDer,
+        SecondDer: secDer
+      }]
+    var columnHead = ["Integral F(x)", "Function f(x)", "First Derivative f'(x)", "Second Derivative f''(x)"];
+    var columnHeadings = Object.keys(data[0]);
     var columnCount = columnHeadings.length;
     var rowCount = data.length;
     var table = $('<table>').appendTo('#output');
     var header = $('<thead />').appendTo(table);
     for (var i = 0; i < columnCount; i++) {
-        $('<th />', { text: columnHeadings[i] }).appendTo(header);
+        $('<th />', { text: columnHead[i] }).appendTo(header);
     }
     var tBody = $('<tbody />').appendTo(table);
     //get row set up right.
+    // Add the data rows to the table body.
+    for (var i = 0; i < rowCount; i++) { // each row
+        var row = $('<tr />').appendTo(tBody);
+        for (var j = 0; j < columnCount; j++) { // each column
+            var obj = data[i];
+            $('<td />', {
+                text: obj[columnHeadings[j]]
+            }).attr('data-label', columnHeadings[j].toUpperCase()).appendTo(row);
+        }
+    }
     document.getElementById("output").style.visibility = "visible";
     document.getElementById("Title").style.background = "lightgreen";
 }
