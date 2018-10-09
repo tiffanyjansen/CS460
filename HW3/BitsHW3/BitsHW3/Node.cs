f4a53002405789a46ddaaf08ea83d72e96721947
tiffany.jansen <tjansen15@wou.edu>
2018-10-09 16:56:24 -0700

    Adding first part of HW3 to folder.

---
+++ HW3/BitsHW3/BitsHW3/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitsHW3
+{
+    /// <summary>
+    /// Singly linked node class.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class Node<T>
+    {
+        public T Data;
+        public Node<T> Next;
+
+        public Node(T Data, Node<T> Next)
+        {
+            this.Data = Data;
+            this.Next = Next;
+        }
+    }
+}
