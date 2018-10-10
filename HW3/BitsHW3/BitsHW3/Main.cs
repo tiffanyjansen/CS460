b832bc1d2fda45cd190670348cb506952832ad0d
tiffany.jansen <tjansen15@wou.edu>
2018-10-09 17:31:26 -0700

    Working on HW3 some more

--- HW3/BitsHW3/BitsHW3/Main.cs
+++ HW3/BitsHW3/BitsHW3/Main.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
 
 namespace BitsHW3
@@ -15,6 +16,59 @@ namespace BitsHW3
     /// </summary>
     class Main
     {
+        /// <summary>
+        /// Print the binary representation of all numbers from 1 up to Number.
+        /// This is accomplished by using a FIFO queue to perform a level 
+        /// order (i.e. BFS) traversal of a virtual binary tree that 
+        /// looks like this:
+        ///                 1
+        ///             /       \
+        ///            10       11
+        ///           /  \     /  \
+        ///         100  101  110  111
+        ///         etc.
+        /// and then storing each "value" in a list as it is "visited".
+        /// </summary>
+        /// <param name="Number">The number we would like to use</param>
+        /// <returns>a LinkedList</returns>
+        static LinkedList<string> GenerateBinaryRepresentationList(int Number)
+        {
+            //Create an empty queue of strings with which to perform the traversal
+            LinkedQueue<StringBuilder> Queue = new LinkedQueue<StringBuilder>();
 
+            //A list for returning the binary values
+            LinkedList<string> Output = new LinkedList<string>();
+
+            if(Number < 1)
+            {
+                //binary representation of negative values is not supported
+                //return an empty list
+                return Output;
+            }
+
+            //Enqueue the first binary number. Use a dynamic string to avoid string concat
+            Queue.push(new StringBuilder("1"));
+
+            //BFS
+            while(Number > 0)
+            {
+                //print out the frot of queue
+                StringBuilder QueueFront = Queue.pop();
+                Output.AddAfter(QueueFront.ToString());
+
+                //Make a copy
+                StringBuilder QueueFrontCopy = new StringBuilder(QueueFront.ToString());
+
+                //Left child
+                QueueFront.Append('0');
+                Queue.push(QueueFront);
+                //Right child
+                QueueFrontCopy.Append('1');
+                Queue.push(QueueFrontCopy);
+
+                Number--;
+            }
+            return Output;
+        }
     }
 }
