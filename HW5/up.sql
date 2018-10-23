dc302c148d7a183285a8aa95cab9ba3de66f823c
tiffany.jansen <tjansen15@wou.edu>
2018-10-22 23:52:40 -0700

    Finished creating table.

---
+++ HW5/up.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[RequestID] INT NOT NULL PRIMARY KEY,
+	FirstName VARCHAR(10)  NOT NULL, 
+	LastName VARCHAR(10) NOT NULL,
+	PhoneNumber VARCHAR(15) NOT NULL,
+	ApartmentName VARCHAR(25) NOT NULL,
+	UnitNumber INT NOT NULL,
+	MaintenanceRequired VARCHAR(100) NOT NULL,
+	TimeOfRequest DATETIME NOT NULL
+)
