2798c16da04a459f9a2c1be809e449c85f587539
tiffany.jansen <tjansen15@wou.edu>
2018-10-24 09:59:47 -0700

    Started on Model Class.

---
+++ HW5/CampusApartments/CampusApartments/Models/Request.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CampusApartments.Models
+{
+    public class Request
+    {
+        [Required]
+        [StringLength(25)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string ApartmentName { get; set; }
+
+        [Required]
+        public int UnitNumber { get; set; }
+
+        [Required]
+        public string MaintenanceRequired { get; set; }
+
+        public DateTime TimeOfRequest
+        {
+            get
+            {
+                var today = DateTime.Today;
+                return today;
+            }
+        }
+
+        public int Permission
+        {
+            get
+            {
+                int permission = 0;
+                return permission;
+            }
+        }
+    }
+}
