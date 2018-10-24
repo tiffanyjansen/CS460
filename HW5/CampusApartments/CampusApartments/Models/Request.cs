54baac9b8b691e28df164e0d16470d2f68df3c73
tiffany.jansen <tjansen15@wou.edu>
2018-10-24 13:32:46 -0700

    Formatting for the form.

--- HW5/CampusApartments/CampusApartments/Models/Request.cs
+++ HW5/CampusApartments/CampusApartments/Models/Request.cs
@@ -10,21 +10,26 @@ namespace CampusApartments.Models
     {
         [Required]
         [StringLength(25)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required]
         [StringLength(25)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         [Required]
         [StringLength(15)]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
 
         [Required]
         [StringLength(15)]
+        [Display(Name = "Apartment Name")]
         public string ApartmentName { get; set; }
 
         [Required]
+        [Display(Name = "Unit Number")]
         public int UnitNumber { get; set; }
 
         [Required]
