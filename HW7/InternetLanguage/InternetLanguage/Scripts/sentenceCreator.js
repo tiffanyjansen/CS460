187401316c334b2f24a7fb17ec080ae9d755c7bd
tiffany.jansen <tjansen15@wou.edu>
2018-11-08 14:32:35 -0800

    Trying to figure out Javascript stuff

---
+++ HW7/InternetLanguage/InternetLanguage/Scripts/sentenceCreator.js
@@ -0,0 +1,10 @@
+﻿$("#words").keypress(function (e) {
+    var word = $("#words").value;
+    console.log(word);
+
+    if (e.keyCode == 0 || e.keyCode == 32)
+    {
+        console.log("space");
+    }
+});
+
