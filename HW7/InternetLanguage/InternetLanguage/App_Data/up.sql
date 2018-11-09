7b6aad6ed36cac9072ac9677c696fbe3831528e6
tiffany.jansen <tjansen15@wou.edu>
2018-11-09 11:38:32 -0800

    Added database/model

---
+++ HW7/InternetLanguage/InternetLanguage/App_Data/up.sql
@@ -0,0 +1,9 @@
+﻿--Requests Table
+CREATE TABLE [dbo].[Requests]
+(
+	[RequestID]        INT IDENTITY (1,1)    NOT NULL,
+    [DateOfRequest]		DateTime	NOT NULL,
+	[IPAddress]		NVARCHAR(25)	NOT NULL,
+	[Browser]		NVARCHAR(25)	NOT NULL
+    CONSTRAINT [PK_dbo.Requests] PRIMARY KEY CLUSTERED ([RequestID] ASC)
+);
