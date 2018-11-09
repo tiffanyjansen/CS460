9a5a10b71fcd6182fe12e33016c5b7e15c25e274
tiffany.jansen <tjansen15@wou.edu>
2018-11-08 23:46:50 -0800

    Added some routing...

--- HW7/InternetLanguage/InternetLanguage/App_Start/RouteConfig.cs
+++ HW7/InternetLanguage/InternetLanguage/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace InternetLanguage
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+               name: "API",
+               url: "API/Sentence/{word}"
+               );
+            
+
+           routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
