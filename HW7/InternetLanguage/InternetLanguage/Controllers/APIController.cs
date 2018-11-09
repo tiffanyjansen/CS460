330a72d5086b3c940cf32432203b15b308ea55bc
tiffany.jansen <tjansen15@wou.edu>
2018-11-08 16:57:26 -0800

    Working on more stuff

---
+++ HW7/InternetLanguage/InternetLanguage/Controllers/APIController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InternetLanguage.Controllers
+{
+    public class APIController : Controller
+    {
+        // GET: API
+        public JsonResult Sentence(string word)
+        {
+            string key = System.Configuration.ConfigurationManager.AppSettings["GiphyKey"];
+
+            Debug.WriteLine("word = " + word);
+            Debug.WriteLine("Key = " + key);
+
+            string website = "https://api.giphy.com/v1/stickers/translate?api_key=" + key + "&s=" + word;
+
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(website);
+            request.Method = WebRequestMethods.Http.Get;
+            request.Accept = "application/json";
+
+            return Json(request, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
