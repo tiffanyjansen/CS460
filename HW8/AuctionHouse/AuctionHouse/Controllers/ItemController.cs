2f119d7b8e7db73c7f950665ebce9f2eebf88b5c
tiffany.jansen <tjansen15@wou.edu>
2018-11-14 14:38:27 -0800

    Started on the list

---
+++ HW8/AuctionHouse/AuctionHouse/Controllers/ItemController.cs
@@ -0,0 +1,20 @@
+﻿using AuctionHouse.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuctionHouse.Controllers
+{
+    public class ItemController : Controller
+    {
+        AntiquitiesContext db = new AntiquitiesContext();
+
+        // GET: Item
+        public ActionResult List()
+        {
+            return View(db.Items);
+        }
+    }
+}
