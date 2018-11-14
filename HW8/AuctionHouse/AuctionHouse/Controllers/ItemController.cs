ddae2391e31903c202cc1747245313e1e9f4658e
tiffany.jansen <tjansen15@wou.edu>
2018-11-14 15:07:06 -0800

    Got started on the Create page

--- HW8/AuctionHouse/AuctionHouse/Controllers/ItemController.cs
+++ HW8/AuctionHouse/AuctionHouse/Controllers/ItemController.cs
@@ -1,4 +1,5 @@
 ﻿using AuctionHouse.DAL;
+using AuctionHouse.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,30 @@ namespace AuctionHouse.Controllers
     {
         AntiquitiesContext db = new AntiquitiesContext();
 
-        // GET: Item
+        [HttpGet]
         public ActionResult List()
         {
+            //return view with item list
             return View(db.Items);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            //return view
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Name,Description,Seller")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Items.Add(item);
+                db.SaveChanges();
+                return View();
+            }
+            return View();
         }
     }
 }
