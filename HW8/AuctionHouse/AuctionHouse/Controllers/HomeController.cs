071eae652f4231b41e89ab2eda2f4a3932ea628e
tiffany.jansen <tjansen15@wou.edu>
2018-11-23 12:49:46 -0800

    May or may not have table done

--- HW8/AuctionHouse/AuctionHouse/Controllers/HomeController.cs
+++ HW8/AuctionHouse/AuctionHouse/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using AuctionHouse.DAL;
+using AuctionHouse.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +12,26 @@ namespace AuctionHouse.Controllers
 {
     public class HomeController : Controller
     {
+        AntiqueContext db = new AntiqueContext();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult Table()
+        {
+            Debug.WriteLine("We are in the Ajax Controller");
+
+            IEnumerable<Bid> recentBids = db.Items.SelectMany(item => item.Bids)
+                .OrderBy(bid => bid.Timestamp)
+                .Take(10);
+
+            var viewModel = recentBids.Select(bid => new { ItemID = bid.Item, ItemName = bid.Item1.Name, BuyerName = bid.Buyer, BidAmount = bid.Price, TimeStamp = bid.Timestamp });
+            
+            string result = JsonConvert.SerializeObject(viewModel, Formatting.None);
+
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ HW8/AuctionHouse/AuctionHouse/Scripts/itemTable.js
@@ -0,0 +1,43 @@
+﻿var interval = 5000;
+console.log("interval = " + interval);
+window.setInterval(ajax_call, interval);
+
+var ajax_call = function () {
+    clearTable();
+    var source = "/Home/Table/";
+    console.log(source);
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: source,
+        success: successAjax,
+        error: errorAjax
+    });
+}
+
+function successAjax(result) {
+    console.log("IT WORKED");
+    var json = JSON.parse(result);
+    console.log(json);
+    $('#Items table').append('<tbody>');
+    success = true;
+    i = 0;
+    while (i < json.length) {
+        $('#Items table').append('<tr><td>' + json[i]["ItemID"] + '</td><td>' + json[i]["ItemName"] + '</td><td>' + json[i]["BuyerName"] + '</td><td>' + json[i]["BidAmount"] + '</td><td>' + json[i]["TimeStamp"] + '</td></tr>');
+        i++;
+    }
+    $('#Items table').append('</tbody>');
+}
+function errorAjax() {
+    console.log("Error");
+}
+
+function createTable() {
+    $('#Items').append('<table align="center" style="width: 90%;"/>');
+    $('#Items table').append('<thead><tr><th>' + 'Item ID' + '</th><th>' + 'Item Name' + '</th><th>' + "Buyer" + '</th><th>' + "Bid Amount" + '</th><th>' + "Time of Bid" + '</th></tr></thead>');
+}
+
+function clearTable() {
+    $('#Items table').remove();
+    createTable();
+}
